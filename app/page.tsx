b2411bff00be023117fc69be4f1f87dce0b3f45a
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-08-10 01:16:07 +0300

    💊 fix: adding styles for project

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="underline">hello world</h1>
+      <h1 className="underline text-white">hello world</h1>
     </div>
   );
 }
