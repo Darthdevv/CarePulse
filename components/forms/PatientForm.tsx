a528c309ef2624039979440648066920c75f61bd
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-08-24 16:28:12 +0300

    💊 chore: adding react-datepicker and react-phone-input

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,16 +4,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -44,21 +46,30 @@ const PatientForm = () => {
           <h1 className="header">Hi there 👋🏻</h1>
           <p className="text-dark-700">Schedule your firrst appointment.</p>
         </section>
-        <FormField
+        <CustomFormField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          label="Email"
+          placeholder="johndoe@@2gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
         />
         <Button type="submit">Submit</Button>
       </form>
